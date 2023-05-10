53a053a1788a9cee816a6e6c62298c7f960d7314
andrejdobric <72233492+andrejdobric@users.noreply.github.com>
2023-05-10 12:40:09 +0200

    Add files via upload

--- front/js/request.js
+++ front/js/request.js
@@ -31,7 +31,7 @@ function GetMovies(){
                 for(var a in response[i].genre) {
                     genreString += `<button class="tag-button" onclick="GetMoviesGenreFilter('${response[i].genre[a]}')">${response[i].genre[a]}</button>`;
                 }
-                $("#parent").append(`<div class="movie${(i%3+1)}" id=${response[i].id}> <h2 class="title">${response[i].title} (${response[i].year})</h2> <p class="description">${response[i].director}</p> <p class="description">${response[i].actors.join(", ")}</p> <div class="genre-tags">${genreString}</div></div>`);
+                $("#parent").append(`<div class="movie${(i%3+1)}" id=${response[i]._id}><div class="delete-movie">✖</div><h2 class="title">${response[i].title} (${response[i].year})</h2> <p class="description">${response[i].director}</p> <p class="description">${response[i].actors.join(", ")}</p> <div class="genre-tags">${genreString}</div></div>`);
             }
         }
     })
@@ -87,6 +87,8 @@ function PostLogin(username, password){
 
     var User = {"username" : username, "password" : password}
 
+    console.log(User);
+
     $.ajax({
         url: `${uri}/login`,
         type: 'POST',
@@ -94,6 +96,25 @@ function PostLogin(username, password){
         contentType: 'application/json',
         success: () => {location.reload()}
         })
+}
+
+function PostMovie(Movie){
+    console.log(Movie);
+
+    $.ajax({
+        url: `${uri}/movies`,
+        type: 'POST',
+        data: JSON.stringify(Movie),
+        contentType: 'application/json',
+        success: () => {location.reload()}
+        })
+}
+
+function DeleteMovie(_id){
+    $.ajax({
+        url: `${uri}/movies/${_id}`,
+        type: 'DELETE',
+    });
 }
 
 function SetDate(){
